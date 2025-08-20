3df76a2be0cf561315737f7e299a548c541468e0
Yash Agarwal <yagarwa2@ncsu.edu>
2025-08-20 19:16:13 -0400

    Completed data processing

--- backend/create_data.py
+++ backend/create_data.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 import mediapipe as mp
 import cv2
@@ -12,22 +13,27 @@ hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 DATA_DIR = './data'
 
+data = []
+labels = []
+
 for dir in os.listdir(DATA_DIR):
     for img_path in os.listdir(os.path.join(DATA_DIR, dir))[:1]:
+        data_aux = []
         img = cv2.imread(os.path.join(DATA_DIR, dir, img_path))
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         results = hands.process(img_rgb)
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    img_rgb, #image to draw
-                    hand_landmarks, #model output
-                    mp_hands.HAND_CONNECTIONS, #hand connections
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())                
+               for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x)
+                    data_aux.append(y)
+                
+            data.append(data_aux)
+            labels.append(dir)
 
-        plt.figure()
-        plt.imshow(img_rgb)
-
-plt.show()
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
